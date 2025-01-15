e636494deb7e84ce8c5c492d6b44790c24ff6ac6
lahh <raihanfazasabiqr@gmail.com>
2025-01-15 16:04:43 +0700

    adding paseto

---
+++ app/auth/paseto.py
@@ -0,0 +1,21 @@
+import time
+
+import pyseto
+
+from utils.utils import private_key, public_key
+
+
+def generate_paseto_token(user_id):
+    now = int(time.time())
+    payload = {
+        "sub": user_id,
+        "iat": now,
+        "exp": now + 3600,
+    }
+    token = pyseto.encode(key=private_key, payload=payload)
+    return token
+
+
+def decode_paseto_token(token: bytes):
+    content = pyseto.decode(keys=public_key, token=token)
+    return content
