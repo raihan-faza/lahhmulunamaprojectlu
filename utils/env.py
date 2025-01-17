cfb1027f94f67466fcdcec1b3d9821128b1389e3
lahh <raihanfazasabiqr@gmail.com>
2025-01-17 18:57:44 +0700

    updating utils

--- utils/env.py
+++ utils/env.py
@@ -1,7 +1,10 @@
-from os import getenv
+import os
 
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(
+    dotenv_path=os.path.join(os.path.dirname(os.path.dirname(__file__)), ".env")
+)
 
-port = getenv("port")
+port = os.getenv("PORT", "8000")
+uri = "ws://localhost:" + str(port)
