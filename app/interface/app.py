77d91ee825485545d84d3bdcc0083f71588a1ba8
lahh <raihanfazasabiqr@gmail.com>
2025-01-20 22:45:21 +0700

    adding footer widget

--- app/interface/app.py
+++ app/interface/app.py
@@ -1,5 +1,38 @@
-from textual.app import App
+from textual.app import App, ComposeResult
+from textual.binding import Binding
+from textual.widgets import Footer
 
 
 class Lahh(App):
-    pass
+    BINDINGS = [
+        Binding(key="q", action="quit", description="Quit the app"),
+        Binding(
+            key="question_mark",
+            action="help",
+            description="Show help screen",
+            key_display="?",
+        ),
+        Binding(
+            key="delete",
+            action="delete",
+            description="Delete the thing",
+        ),
+        Binding(
+            key="j",
+            action="down",
+            description="Scroll down",
+        ),
+        Binding(
+            key="k",
+            action="up",
+            description="Scroll up",
+        ),
+        Binding(
+            key="c",
+            action="",
+            description="Go to chat bar",
+        ),
+    ]
+
+    def compose(self) -> ComposeResult:
+        yield Footer()
--- main.py
+++ main.py
@@ -7,7 +7,5 @@ from app.utils.db import init
 from app.ws.server import start_server
 
 if __name__ == "__main__":
-    run_async(init())
-    # asyncio.run(start_server())
-    # app = Lahh()
-    # app.run()
+    app = Lahh()
+    app.run()
